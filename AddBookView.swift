979ee95e88644c71418f6ce559be4ee581bc592b
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-21 19:16:36 +0200

    :notebook: Add a custom star rating component

--- AddBookView.swift
+++ AddBookView.swift
@@ -35,12 +35,8 @@ struct AddBookView: View {
                 
                 Section{
                     TextEditor(text: $review)
-                    
-                    Picker("Rating", selection: $rating){
-                        ForEach(0..<6){
-                            Text(String($0))
-                        }
-                    }
+                    RatingView(rating: $rating)
+                        .frame(maxWidth: .infinity, alignment: .center)
                 } header: {
                     Text("Write a review")
                 }
