9995a89c99aba9ffccc1da125b2494b75345108a
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-21 15:32:17 +0200

    Initial commit

---
+++ BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Pat on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BookWorm
+ Swiftui app
