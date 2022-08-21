72ad8d87954b6d7610f5c0f1b63f7e48f3cfd6f1
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-21 20:03:09 +0200

    :notebook:  Adding a detail view for the core data objects
    When adding a detail view add the preview part to your persistence controller so it shows when you need it

---
+++ DetailView.swift
@@ -0,0 +1,47 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Pat on 2022/08/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    var body: some View {
+        ScrollView{
+            ZStack(alignment: .bottomTrailing){
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaleEffect()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            Text(book.author ?? "")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+//struct DetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailView(book: )
+//    }
+//}
